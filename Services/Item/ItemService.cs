9b7da572761260eb00a3be64f3792754a2ca449a
Shadman Sakib <shadmanabir@outlook.com>
2024-09-23 13:42:09 +0600

    item service added

---
+++ Services/Item/ItemService.cs
@@ -0,0 +1,15 @@
+﻿using SCM.Model;
+
+namespace SCM.Services.Item
+{
+    public class ItemService
+    {
+        private readonly SCMContext _context;
+        public ItemService(SCMContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
