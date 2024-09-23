57a92d6920572c571df315a37af0c83b31d2298f
Shadman Sakib <shadmanabir@outlook.com>
2024-09-23 12:33:12 +0600

    Context class added

---
+++ Model/SCMContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SCM.Model.Items;
+using SCM.Model.Requisitions;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace SCM.Model
+{
+    public class SCMContext : IdentityDbContext
+    {
+        public SCMContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category_tbl> Category_tbl { get; set; }
+        public DbSet<ItemBase_tbl> ItemBase_tbl { get; set; }
+        public DbSet<ItemMaster_tbl> BorrowdItemMaster_tblBooks { get; set; }
+        public DbSet<RequisitionMaster_tbl> RequisitionMaster_tbl { get; set; }
+        public DbSet<RequisitionDetail_tbl> RequisitionDetail_tbl { get; set; }
+    }
+}
