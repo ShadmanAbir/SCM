1c5f3f343c10fb898c6f8cc158f231c673e59134
Shadman Sakib <shadmanabir@outlook.com>
2024-09-23 12:19:44 +0600

    ItemMaster Model Added

---
+++ Model/ItemMaster_tbl.cs
@@ -0,0 +1,9 @@
+﻿namespace SCM.Model
+{
+    public class ItemMaster_tbl:BaseEntity
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
