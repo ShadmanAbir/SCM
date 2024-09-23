b4fbd237159ebd6280bedb2027f01dd45c57ca71
Shadman Sakib <shadmanabir@outlook.com>
2024-09-23 12:12:40 +0600

    init

---
+++ Model/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace SCM.Model
+{
+    public class BaseEntity
+    {
+        public bool? IsDeleted { get; set; }
+        public bool? IsApproved { get; set; }
+        public int? Status { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+    }
+}
