fbfac90af5f157a79fd61d96732ac1618fbf53ea
Shadman Sakib <shadmanabir@outlook.com>
2024-09-23 12:22:03 +0600

    Requisition Master added

---
+++ Model/RequisitionMaster_tbl.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SCM.Model
+{
+    public class RequisitionMaster_tbl
+    {
+        [Key]
+        public int RequisitionID { get; set; }
+
+        [Required]
+        [StringLength(30)] 
+        public string RequisitionNo { get; set; }
+        [Column(TypeName = "date")] 
+        public DateTime RequisitionDate { get; set; }
+        [Column(TypeName = "date")] 
+        public DateTime EstimatedRequiredDate { get; set; }
+        [Required][StringLength(500)] 
+        public string Note { get; set; }
+    }
+}
