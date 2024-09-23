a965baa675bece3959946d2cab8ea7170eb8ffed
Shadman Sakib <shadmanabir@outlook.com>
2024-09-23 12:28:32 +0600

    Category Model added

---
+++ Model/Items/Category_tbl.cs
@@ -0,0 +1,9 @@
+﻿namespace SCM.Model.Items
+{
+    public class Category_tbl : BaseEntity
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public int ParentID { get; set; }
+    }
+}
